fbf82721c72499ba170ef10521b417e6fa49d860
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-13 15:47:54 +0530

    Header component added

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Dish here!!</h2>
+        <p>
+          Craving something tasty? We deliver a wide variety of delicious foods
+          right to your doorstep. From spicy to sweet, every dish is made to
+          satisfy your taste buds. Enjoy fresh, flavorful meals anytime,
+          anywhere!
+        </p>
+
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
