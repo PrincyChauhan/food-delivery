c0f1832589affe2ec9cbd2172d4a4688f4003290
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-14 11:33:09 +0530

    MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = 4000;
@@ -7,6 +8,8 @@ const port = 4000;
 //middleware
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
