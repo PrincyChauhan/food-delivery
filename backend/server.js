114573eef2b07ec92d60046d48e19c10bb546ba5
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-14 10:58:04 +0530

    Server started

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
