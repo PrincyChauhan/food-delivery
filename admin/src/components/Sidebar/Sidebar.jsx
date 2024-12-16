1f6b518c3d44623b90947e6e17373f78e900a9e0
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-16 12:02:34 +0530

    Admin side add, list and order route added

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,23 @@
 import "./sidebar.css";
 import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-options">
-        <div className="sidebar-option">
+        <NavLink to="/add" className="sidebar-option">
           <img src={assets.add_icon} alt="add_icon" />
           <p>Add Item</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to="/list" className="sidebar-option">
           <img src={assets.order_icon} alt="order_icon" />
           <p>List Item</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to="/orders" className="sidebar-option">
           <img src={assets.order_icon} alt="order_icon" />
           <p>Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
