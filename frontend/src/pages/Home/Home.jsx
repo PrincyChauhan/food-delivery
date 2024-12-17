4ae47e9e40800dc9fca23f4feb07801235f969a1
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-17 11:48:53 +0530

    Changes in Exploremenu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 const Home = () => {
-  const [category, setCategory] = useState("all");
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
