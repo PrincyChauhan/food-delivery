07a5f66ff001bd12be1196490e4deac6b1c70357
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-17 15:42:56 +0530

    Link added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("Menu");
@@ -9,30 +10,38 @@ const Navbar = () => {
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li
+        <Link
+          to="/"
           onClick={() => setMenu("Home")}
           className={menu === "Home" ? "active" : ""}
         >
-          Home
-        </li>
-        <li
+          {" "}
+          Home{" "}
+        </Link>
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("Menu")}
           className={menu === "Menu" ? "active" : ""}
         >
-          Menu
-        </li>
-        <li
+          {" "}
+          Menu{" "}
+        </a>
+        <a
+          href="#app-download"
           onClick={() => setMenu("Mobile App")}
           className={menu === "Mobile App" ? "active" : ""}
         >
-          Mobile App
-        </li>
-        <li
+          {" "}
+          Mobile App{" "}
+        </a>
+        <a
+          href="#footer"
           onClick={() => setMenu("Contact Us")}
           className={menu === "Contact Us" ? "active" : ""}
         >
-          Contact Us
-        </li>
+          {" "}
+          Contact Us{" "}
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search_icon" />
