4331aacd8d362437e12273550df19e8d895ea3e4
PrincyChauhan <chauhanprincee7@gmail.com>
2024-12-24 17:28:06 +0530

    Add to cart and remove form cart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -10,15 +10,40 @@ const StoreContextProvider = (props) => {
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
-  const addToCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: (prev[itemId] || 0) + 1 }));
+  // const addToCart = (itemId) => {
+  //   setCartItems((prev) => ({ ...prev, [itemId]: (prev[itemId] || 0) + 1 }));
+  // };
+
+  const addToCart = async (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+    if (token) {
+      await axios.post(
+        url + "/api/cart/add",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
-  const removeFromCart = (itemId) => {
-    setCartItems((prev) => {
-      const newCount = (prev[itemId] || 0) - 1;
-      return { ...prev, [itemId]: Math.max(newCount, 0) }; // Prevent negative values
-    });
+  // const removeFromCart = (itemId) => {
+  //   setCartItems((prev) => {
+  //     const newCount = (prev[itemId] || 0) - 1;
+  //     return { ...prev, [itemId]: Math.max(newCount, 0) }; // Prevent negative values
+  //   });
+  // };
+  const removeFromCart = async (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + "/api/cart/remove",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
   const getTotalCartAmount = () => {
